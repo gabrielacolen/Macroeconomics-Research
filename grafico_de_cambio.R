a92d3da05dfe9418274ace7150a81acd3203642a
Gabriela Colem Castelo Borges <73849139+gabrielacolen@users.noreply.github.com>
2025-02-18 18:09:51 -0300

    Update grafico_de_cambio.R

--- grafico_de_cambio.R
+++ grafico_de_cambio.R
@@ -4,14 +4,13 @@ library(ggplot2)
 
 taxa_cambio_compra = get_series(10813, start_date = "2024-01-01")
 
-# Certifique-se de que o dataframe cambio_compra está correto
 cambio_compra <- taxa_cambio_compra %>%
   rename(data = date, taxa_cambio = "10813") %>%
   mutate(ano_mes = format(data, "%Y-%m"))
 
 # Plotar o gráfico
-ggplot(cambio_compra, aes(x = data, y = taxa_cambio)) +  # Use taxa_cambio, não cambio_compra
-  geom_line(color = "blue", linewidth = 1.5) +  # Use linewidth em vez de size
+ggplot(cambio_compra, aes(x = data, y = taxa_cambio)) + 
+  geom_line(color = "blue", linewidth = 1.5) +  
   labs(
     title = "Taxa de Câmbio de Compra (USD/BRL)",  # Título do gráfico
     x = "Data",  # Rótulo do eixo X
