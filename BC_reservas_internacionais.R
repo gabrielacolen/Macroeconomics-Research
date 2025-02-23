7cd2e57abaa838ba9960b82bfad8886cfb2a6a37
Gabriela Colem Castelo Borges <73849139+gabrielacolen@users.noreply.github.com>
2025-02-23 19:33:04 -0300

    Update BC_reservas_internacionais.R

--- BC_reservas_internacionais.R
+++ BC_reservas_internacionais.R
@@ -1,5 +1,3 @@
-install.packages("scales")
-
 library(rbcb)
 library(dplyr)
 library(ggplot2)
@@ -12,7 +10,7 @@ reservas_renomeado <- reservas %>%
   mutate(ano_mes = format(data, "%Y-%m")) # Criar coluna Ano-Mês
 
 # Plotar o gráfico com valores em milhões de reais
-ggplot(reservas_renomeado, aes(x = data, y = reservas / 1e6)) +  # Divide os valores por 1 milhão
+ggplot(reservas_renomeado, aes(x = data, y = reservas / 1e3)) +  # Divide os valores por 1 milhão
   geom_line(color = "blue") +  # Linha do gráfico
   labs(title = "Reservas Internacionais Banco Central",  # Título do gráfico
        y = "Reservas (em milhões de dólares)") +  # Rótulo do eixo Y
